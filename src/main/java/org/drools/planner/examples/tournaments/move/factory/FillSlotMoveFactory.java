9db50b0e7a825e2cd38bb286782916edb144287d
Martin Večeřa <marvenec@gmail.com>
2011-11-28 23:40:12 +0100

    fixing empty slot filling

--- src/main/java/org/drools/planner/examples/tournaments/move/factory/FillSlotMoveFactory.java
+++ src/main/java/org/drools/planner/examples/tournaments/move/factory/FillSlotMoveFactory.java
@@ -11,19 +11,33 @@ import org.drools.planner.examples.tournaments.model.Match;
 import org.drools.planner.examples.tournaments.model.Slot;
 import org.drools.planner.examples.tournaments.move.FillSlotMove;
 
-
 public class FillSlotMoveFactory extends CachedMoveFactory {
 
-    @Override
-    public List<Move> createCachedMoveList(@SuppressWarnings("rawtypes") Solution arg0) {
-        TournamentsSolution sol = (TournamentsSolution) arg0;
-        List<Move> moves = new LinkedList<>();
-        for (Slot s: sol.getSlotList()) {
-            for (Match m : sol.getMatchList()) {
-                moves.add(new FillSlotMove(m, s));
-            }
-        }
-        return moves;
-    }
+	@Override
+	public List<Move> createCachedMoveList(
+			@SuppressWarnings("rawtypes") Solution arg0) {
+		TournamentsSolution sol = (TournamentsSolution) arg0;
+		List<Move> moves = new LinkedList<>();
+		List<Match> used = new LinkedList<>();
+		List<Slot> unusedSlots = new LinkedList<>();
+		for (Slot s : sol.getSlotList()) {
+			if (s.getMatch() != null) {
+				used.add(s.getMatch());
+			} else {
+				unusedSlots.add(s);
+			}
+		}
+		for (Slot s : unusedSlots) {
+			if (s.getMatch() == null) {
+				for (Match m : sol.getMatchList()) {
+					if (!used.contains(m)) {
+						moves.add(new FillSlotMove(m, s));
+						used.add(m);
+					}
+				}
+			}
+		}
+		return moves;
+	}
 
 }
